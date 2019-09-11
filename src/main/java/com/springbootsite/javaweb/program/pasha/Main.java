0ad0681f7a59c396933482142ee9c379ffc752b8
topprogereu <topprogereu@gmail.com>
2019-09-11 17:20:17 +0300

    Start commit

---
+++ src/main/java/com/springbootsite/javaweb/program/pasha/Main.java
@@ -0,0 +1,15 @@
+package com.springbootsite.javaweb.program.pasha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+
+
+}
---
+++ src/main/resources/static/js/test.js
@@ -0,0 +1,9 @@
+function fact( a )
+{
+    res = 1;
+    for (i=1;i<a;i++)
+        res *= i;
+    return res;
+
+}
+alert(fact(5));
