f26e8f0ff81d9e0ea930d4798fbc4a6850b0f1a9
topprogereu <topprogereu@gmail.com>
2019-09-18 10:16:38 +0300

    Test version

--- src/main/java/com/springbootsite/javaweb/program/pasha/Main.java
+++ src/main/java/com/springbootsite/javaweb/program/pasha/Main.java
@@ -3,12 +3,15 @@ package com.springbootsite.javaweb.program.pasha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class,args);
+        SpringApplication springApplication = new SpringApplication(Main.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
     }
 
 
